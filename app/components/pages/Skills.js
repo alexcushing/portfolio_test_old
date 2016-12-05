d8550e6746f07c7f04c29074d2c1f9d96da48a9a
Alex Cushing <alexander_cushing@student.uml.edu>
2016-12-05 11:38:20 -0500

    alignment update

--- app/components/pages/Skills.js
+++ app/components/pages/Skills.js
@@ -6,82 +6,76 @@ import Contact from 'app/components/Contact';
 
 const Skills = () => {
   return (
-  <div>
-    <Nav name="Skills" />
-    <div className="panel panel-default limitWidth homeBox">
-      <div className="mainSkills">
-        <div className="skillBlock">
-          <h2 className="skillTitle expert">Expert</h2>
-        <hr className ="line exp" />
-        <div className="expert">
-          <h2>Expert</h2>
-          <ul>
-            <li>
-              C/C++
-            </li>
-              <li>
-                Python
-              </li>
-            <li>
-              JavaScript
-            </li>
-            <li>
-              JQuery
-            </li>
-            <li>
-              CSS3 / SASS / SCSS
-            </li>
-      
-              <li>
-                Django
-              </li>
-            <li>
-              HTML5
-            </li>
-            <li>
-              Bootstrap
-            </li>
-          </ul>
-        </div>
-        <div className="skillBlock">
-          <h2 className="skillTitle proficient">Proficient</h2>
-        <hr className ="line prof"/>
-          <ul>
-        <div className="proficient">
-          <h2>Proficient</h2>
-          <ul>
-              <li>
-                Django
-              </li>
-            <li>
-              Jade / Pug
-            </li>
-            <li>
-              MongoDB
-            </li>
-          </ul>
-        </div>
-        <div className="skillBlock">
-          <h2 className="skillTitle competent">Competent</h2>
-            <hr className ="line comp"/>
-        <div className="competent">
-          <h2>Competent</h2>
-          <ul>
-            <li>
-              ReactJS
-            </li>
-              <li>
-                NodeJS
-              </li>
-            <li>
-              SQL
-            </li>
-          </ul>
-          <Contact />
+      <div>
+        <Nav name="Skills" />
+        <div className="panel panel-default limitWidth homeBox">
+          <div className="mainSkills">
+            <div className="skillBlock">
+              <h2 className="skillTitle expert">Expert</h2>
+            <hr className ="line exp" />
+              <ul>
+                <li>
+                  C/C++
+                </li>
+                  <li>
+                    Python
+                  </li>
+                <li>
+                  JavaScript
+                </li>
+                <li>
+                  JQuery
+                </li>
+                <li>
+                  CSS3 / SASS / SCSS
+                </li>
+
+                  <li>
+                    Django
+                  </li>
+                <li>
+                  HTML5
+                </li>
+                <li>
+                  Bootstrap
+                </li>
+              </ul>
+            </div>
+            <div className="skillBlock">
+              <h2 className="skillTitle proficient">Proficient</h2>
+            <hr className ="line prof"/>
+              <ul>
+                  <li>
+                    Django
+                  </li>
+                <li>
+                  Jade / Pug
+                </li>
+                <li>
+                  MongoDB
+                </li>
+              </ul>
+            </div>
+            <div className="skillBlock">
+              <h2 className="skillTitle competent">Competent</h2>
+                <hr className ="line comp"/>
+              <ul>
+                <li>
+                  ReactJS
+                </li>
+                  <li>
+                    NodeJS
+                  </li>
+                <li>
+                  SQL
+                </li>
+              </ul>
+              <Contact />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>);
+  );
 }
 
 export default Skills;
